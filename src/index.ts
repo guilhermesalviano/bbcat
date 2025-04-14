16d91080d013122dc20b9b487630d3e2d94d99be
guilhermesalviano <guilherme.salviano12@outlook.com>
2025-04-14 19:33:51 -0300

    feat: setup socketio and htmls

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,39 @@ import { createServer } from 'http';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './routes';
-
+import { Server } from 'socket.io';
 dotenv.config();
 
 const app = express();
 const httpServer = createServer(app);
+const io = new Server(httpServer);
 
 // Configuração do CORS
 app.use(cors());
 app.use(express.json());
-app.use(router)
+app.use(router);
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId) => {
+    console.log("User joined room:", roomId, userId);
+    socket.join(roomId);
+    socket.to(roomId).emit('user-connected', userId);
+  });
+
+  socket.on('offer', (offer, roomId) => {
+    socket.to(roomId).emit('offer', offer, socket.id);
+  });
+
+  socket.on('answer', (answer, roomId) => {
+    console.log("Answer:", answer);
+    socket.to(roomId).emit('answer', answer, socket.id);
+  });
+
+  socket.on('ice-candidate', (candidate, roomId) => {
+    socket.to(roomId).emit('ice-candidate', candidate, socket.id);
+  });
+});
 
 // Inicia o servidor
 const PORT = process.env.PORT || 3000;
