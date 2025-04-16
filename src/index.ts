c8a30ea30fcb4e71c7f370670b6be204b5e60904
guilhermesalviano <guilherme.salviano12@outlook.com>
2025-04-16 20:13:18 -0300

    style: refactor indentations

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ import { Server } from 'socket.io';
 dotenv.config();
 
 const options = {
-  key: fs.readFileSync('./private.key'),
-  cert: fs.readFileSync('./certificate.crt')
+    key: fs.readFileSync('./private.key'),
+    cert: fs.readFileSync('./certificate.crt'),
 };
 
 const app = express();
@@ -23,27 +23,27 @@ app.use(router);
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-  socket.on('join-room', (roomId, userId) => {
-    console.log("User joined room:", roomId, userId);
-    socket.join(roomId);
-    socket.to(roomId).emit('user-connected', userId);
-  });
+    socket.on('join-room', (roomId, userId) => {
+        console.log('User joined room:', roomId, userId);
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected', userId);
+    });
 
-  socket.on('offer', (offer, roomId) => {
-    socket.to(roomId).emit('offer', offer, socket.id);
-  });
+    socket.on('offer', (offer, roomId) => {
+        socket.to(roomId).emit('offer', offer, socket.id);
+    });
 
-  socket.on('answer', (answer, roomId) => {
-    socket.to(roomId).emit('answer', answer, socket.id);
-  });
+    socket.on('answer', (answer, roomId) => {
+        socket.to(roomId).emit('answer', answer, socket.id);
+    });
 
-  socket.on('ice-candidate', (candidate, roomId) => {
-    socket.to(roomId).emit('ice-candidate', candidate, socket.id);
-  });
+    socket.on('ice-candidate', (candidate, roomId) => {
+        socket.to(roomId).emit('ice-candidate', candidate, socket.id);
+    });
 });
 
 // Inicia o servidor
 const PORT = process.env.PORT || 3000;
 httpServer.listen(PORT, () => {
-  console.log(`Servidor rodando em https://{ip_local}:${PORT}`);
+    console.log(`Servidor rodando em https://{ip_local}:${PORT}`);
 });
